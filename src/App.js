9c5807eed93b9b0f126ee4de60f14d7dbef79dcc
Sugeng Arif Barianto <id.barianto@gmail.com>
2022-07-21 21:28:33 +0700

    feat: router pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+
+import NavBar from "./components/NavBar";
+import HomePage from "./containers/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <HomePage />
     </div>
   );
 }
